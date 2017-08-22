ae330c34d8d9eb765176d07651ef6353b45ef84a
auuster <aaron.ks.au.91@gmail.com>
2017-08-22 15:19:20 -0400

    Inital Content
    Arduino Code - Move with esp300 like commands
    
    Python GUI - with buttons to move to focusing points, and acquire images.

---
+++ PlateScannerArduino/PlateScannerArduino.ino
@@ -0,0 +1,99 @@
+ //ARDUINO MOVE with Commands
+//YIP LAB - Aaron Au
+
+//Constants
+int pinDir[] = {6,3}; //Motor Direction {x,y}
+int pinEn[] = {5,2}; //Motor Enable {x,y}
+int pinPul[] = {7,4}; //Motor Pulse {x,y}
+int pinLim[2][2] = {{11,10},{8,9}};//Limit switch X positive direction {{x+,x-},{y+,y-}}
+
+int pulseTime = 300;  //Time between High and Low for a pulse (us)
+float stepsPerMillimeter = 800.*16./25.4; //800 steps/rotation * 16 rotations/inch * 1/25.4 inch/mm 
+
+boolean limitNotHit;
+
+void setup() {
+  Serial.begin(9600);
+
+  for (int i=0; i<2; i++) {
+    pinMode(pinDir[i], OUTPUT);
+    pinMode(pinEn[i], OUTPUT);
+    pinMode(pinPul[i], OUTPUT);
+    pinMode(pinLim[i][0], INPUT_PULLUP);
+    pinMode(pinLim[i][1], INPUT_PULLUP);
+  }
+
+  pinMode(12, OUTPUT);
+  digitalWrite(12, HIGH);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);   
+
+  Serial.println("Move with Commands");
+}
+
+boolean moveSteps(int axis, boolean dir, long nrsteps, boolean limits) {
+  digitalWrite(pinEn[axis], HIGH);
+  digitalWrite(pinDir[axis], dir);
+  for (int i=0; i<nrsteps; i++) {
+    digitalWrite(pinPul[axis], HIGH);
+    delayMicroseconds(pulseTime/2);
+    digitalWrite(pinPul[axis], LOW);
+    delayMicroseconds(pulseTime/2);
+    if (limits && (digitalRead(pinLim[axis][0])==LOW || digitalRead(pinLim[axis][1])==LOW)) {
+      digitalWrite(pinDir[axis], !dir);
+      for (int j=0; j<500; j++) {
+        digitalWrite(pinPul[axis], HIGH);
+        delayMicroseconds(pulseTime/2);
+        digitalWrite(pinPul[axis], LOW);
+        delayMicroseconds(pulseTime/2);
+      }
+      digitalWrite(pinEn[axis], LOW);
+      return false;
+    }
+  }
+  digitalWrite(pinEn[axis], LOW);
+  return true;
+}
+
+void doCommand(String command) {
+  if (command.substring(1,3) == "pr") {
+    //pr = Position Relative
+    int axis = command.substring(0,1).toInt();
+    float distance = command.substring(3,10).toFloat();
+    boolean dir = distance > 0.0;
+    long steps = (long) abs(distance) * stepsPerMillimeter + 0.5;  // + 0.5 for rounding
+    /*Serial.print("axis: ");
+    Serial.print(axis);
+    Serial.print("direction: ");
+    Serial.print(dir);
+    Serial.print(abs(distance)*stepsPerMillimeter);
+    Serial.print("steps: ");
+    Serial.println(steps);*/
+    Serial.println(moveSteps(axis, dir, steps, true));
+  } else if (command.substring(1,3) == "or") {
+    zero();
+  }
+
+}
+
+boolean zero() {
+for (int i = 0; i<2; i++) {  
+  limitNotHit = true;
+  while (limitNotHit) {
+      limitNotHit = moveSteps(i, false, 100,true);
+    }
+  moveSteps(i,true, 500, false);
+}
+Serial.println("or");
+}
+
+void loop() {
+if (Serial.available()) {
+  String command = Serial.readStringUntil(';');
+  String axis;
+  if (command.substring(0,1) == "0" || command.substring(0,1) == "1") {
+    doCommand(command);
+  }
+}
+}
+
---
+++ PlateScannerGUI.py
@@ -0,0 +1,254 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 18 13:31:18 2017
+
+@author: Aaron Au
+"""
+from Tkinter import *
+import tkFileDialog
+import twain
+import serial
+import time
+import os
+from PIL import Image, ImageTk
+
+#VARIABLES
+xyCom = "COM3"; #xy_stage COM port
+well_size = 9; #mm
+start_x = 50; #mm
+start_y = 11; #mm
+com_sleep = 1; #seconds
+cap_sleep = 1; #seconds
+point1_x =4.5*well_size; #mm
+point1_y =0; #mm
+point2_x =0; #mm
+point2_y =11*well_size; #mm
+point3_x =7*well_size; #mm
+point3_y =11*well_size; #mm
+resize = 500,500; #Image Size
+
+#INITIALIZE GUI
+root = Tk()
+root.title("96 Well Plate Scanner")
+root.geometry("500x500")
+
+app = Frame(root)
+app.grid();
+
+labtext = StringVar()
+labstatus = Label(app, textvariable=labtext, width=20, height=1)
+
+cwd = os.getcwd();
+vsave = StringVar()
+labsave = Label(app, textvariable=vsave, width=50, height=1)
+vsave.set(cwd);
+
+labimage = Label(app, image = None);
+
+#INITIALIZE XY STAGE
+xy_stage = serial.Serial(xyCom);
+
+#INITIALIZE CAMERA
+sm = twain.SourceManager(0);
+cwd = os.getcwd();
+ss = sm.open_source();
+
+def sendSerial(com, command):
+    print(command);
+    com.write(command);
+    
+def recSerial(com):
+    return com.readline();
+
+
+def changeStatus(stat):
+    labtext.set(stat);
+    labstatus.update_idletasks();
+
+def changeImage(img):
+    img = ImageTk.PhotoImage(img)
+    labimage.configure(image=img)
+    labimage.image=img
+    labimage.update_idletasks()
+
+
+running = False  # Global flag
+scantime = 0 #Global counter
+zeroed = False # Global indicator
+savedir = cwd #Save Directory
+
+def scanning():
+    global scantime
+    if running:  # Only do this if the Stop button has not been clicked
+        print "scanning"
+        changeStatus('Focusing')
+        ss.request_acquire(0, 0);
+        time.sleep(cap_sleep);
+        sendSerial(xy_stage, "1pr" + str((-1) ** scantime * well_size) + ";\r\n");
+        file_name = 'test.tiff'        
+        ss.file_xfer_params = (file_name, 0);
+        os.remove(cwd + '\\' + file_name);
+        if 'im' in vars():
+            im.close();
+        ss.xfer_image_by_file();
+        ss.hide_ui();
+        im = Image.open(file_name);
+        im.thumbnail(resize, Image.ANTIALIAS)
+        changeImage(im)
+        scantime += 1
+
+    # After 1 second, call scanning again (create a recursive loop)
+    root.after(1000, scanning)
+
+def focus_1():
+    """Enable scanning by setting the global flag to True."""
+    global running
+    global scantime
+    global zeroed
+    if running:
+        print 'Already Running'
+    else: 
+        running = True
+        changeStatus('Focus 1')
+        sendSerial(xy_stage,"0pr"+str(point1_x)+";\r\n");
+        time.sleep(com_sleep);
+        print(recSerial(xy_stage));
+        time.sleep(com_sleep);
+        sendSerial(xy_stage,"1pr"+str(point1_y)+";\r\n");
+        time.sleep(com_sleep);
+        print(recSerial(xy_stage));
+        scantime = 0;
+        zeroed = False;
+        
+def focus_2():
+    """Enable scanning by setting the global flag to True."""
+    global running
+    global scantime
+    global zeroed
+    if running:
+        print 'Already Running'
+    else: 
+        running = True
+        changeStatus('Focus 2')
+        sendSerial(xy_stage,"0pr"+str(point2_x)+";\r\n");
+        time.sleep(com_sleep);
+        print(recSerial(xy_stage));
+        time.sleep(com_sleep);
+        sendSerial(xy_stage,"1pr"+str(point2_y)+";\r\n");
+        time.sleep(com_sleep);
+        print(recSerial(xy_stage));
+        scantime = 0;
+        zeroed = False;
+        
+def focus_3():
+    """Enable scanning by setting the global flag to True."""
+    global running
+    global scantime
+    global zeroed
+    if running:
+        print 'Already Running'
+    else: 
+        running = True
+        changeStatus('Focus 3')
+        sendSerial(xy_stage,"0pr"+str(point3_x)+";\r\n");
+        time.sleep(com_sleep);
+        print(recSerial(xy_stage));
+        time.sleep(com_sleep);
+        sendSerial(xy_stage,"1pr"+str(point3_y)+";\r\n");
+        time.sleep(com_sleep);
+        print(recSerial(xy_stage));
+        scantime = 0;
+        zeroed = False;
+
+def findsavedir():
+    global savedir
+    savedir = tkFileDialog.askdirectory(title="Choose Save Directory", initialdir=cwd)
+    vsave.set(savedir)
+
+
+def stop():
+    """Stop scanning by setting the global flag to False."""
+    global running
+    global zeroed
+    if zeroed == False:
+        xy_stage.reset_input_buffer();
+        time.sleep(1)
+        changeStatus('Zeroing')
+
+        sendSerial(xy_stage, "0or;\r\n");
+        time.sleep(com_sleep);
+        print(recSerial(xy_stage));
+        time.sleep(com_sleep);
+        sendSerial(xy_stage,"0pr"+str(start_x)+";\r\n");
+        time.sleep(com_sleep);
+        print(recSerial(xy_stage));
+        time.sleep(com_sleep);
+        sendSerial(xy_stage,"1pr"+str(start_y)+";\r\n");
+        time.sleep(com_sleep);
+        print(recSerial(xy_stage));
+
+        running = False
+        zeroed = True
+        changeStatus('Ready')
+
+def scan():
+    global running
+    global zeroed
+    global savedir
+    if running:
+        print 'Already Running'
+    else:
+        running = True
+        zeroed = False
+        changeStatus('Scanning Plate')
+        print('scan plate')
+        time.sleep(1)
+        for col in range(8):
+            #Acquire and move
+            for i in range(12):
+                ss.request_acquire(0, 0);
+                time.sleep(cap_sleep);
+                sendSerial(xy_stage, "1pr" + str((-1) ** col * well_size) + ";\r\n");
+                file_name = savedir + '\\'+str(col)+ '_' + str(i) + '.tiff'
+                ss.file_xfer_params = (file_name, 0);
+                os.remove(file_name);
+                ss.xfer_image_by_file();
+                ss.hide_ui();
+                im = Image.open(file_name);
+                im.thumbnail(resize, Image.ANTIALIAS)
+                changeImage(im)
+        
+            #Wait until motor movement is finished:
+            xy_stage.reset_input_buffer();
+            time.sleep(1);
+        
+            if (col < 7): #Move to next row
+                sendSerial(xy_stage, "0pr"+str(well_size)+";\r\n");
+                time.sleep(com_sleep);
+                print(recSerial(xy_stage));
+        
+        stop()
+
+print(recSerial(xy_stage));
+time.sleep(com_sleep)
+stop();
+
+butstart_0 = Button(app, text="Focus 1", command=focus_1)
+butstart_1 = Button(app, text="Focus 2", command=focus_2)
+butstart_2 = Button(app, text="Focus 3", command=focus_3)
+butstop = Button(app, text="Stop", command=stop)
+butscan = Button(app, text="Scan Plate", command=scan)
+butsavedir = Button(app, text="Directory", command=findsavedir)
+
+butstart_0.grid(row=0,column=0)
+butstart_1.grid(row=0,column=1)
+butstart_2.grid(row=0,column=2)
+labstatus.grid(row=1,column=0,columnspan=3)
+butscan.grid(row=2,column=0)
+butstop.grid(row=2, column=1)
+butsavedir.grid(row=3,column=0)
+labsave.grid(row=3, column=1, columnspan=3)
+labimage.grid(row=4, column=0, columnspan=4)
+
+root.after(1000, scanning)  # After 1 second, call scanning
+root.mainloop()
